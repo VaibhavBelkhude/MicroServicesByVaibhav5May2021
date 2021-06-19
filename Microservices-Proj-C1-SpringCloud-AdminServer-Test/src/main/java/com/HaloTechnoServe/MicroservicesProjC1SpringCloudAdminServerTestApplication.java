46c292fa23a5e9d525448cda098c09da13f8ae08
Vaibhav <vaibhavbelkhude555@gmail.com>
2021-06-19 12:12:50 +0530

    AdminServer-UI-Test

---
+++ Microservices-Proj-C1-SpringCloud-AdminServer-Test/src/main/java/com/HaloTechnoServe/MicroservicesProjC1SpringCloudAdminServerTestApplication.java
@@ -0,0 +1,16 @@
+package com.HaloTechnoServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MicroservicesProjC1SpringCloudAdminServerTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProjC1SpringCloudAdminServerTestApplication.class, args);
+	}
+
+}
---
+++ Microservices-Proj-C2-SpringCloud-AdminServer-MS-Employee-Test/src/main/java/com/HaloTechnoServe/MicroservicesProjC2SpringCloudAdminServerMsEmployeeTestApplication.java
@@ -0,0 +1,14 @@
+package com.HaloTechnoServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class MicroservicesProjC2SpringCloudAdminServerMsEmployeeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesProjC2SpringCloudAdminServerMsEmployeeTestApplication.class, args);
+	}
+
+}
